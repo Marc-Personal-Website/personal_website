684cdc7829947c6efc1c911aadb301cb6be1d1b0
Marc Grupe <marc.grupe@gmail.com>
2025-03-20 11:20:30 -0400

    updating contact page

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('#form')
+const submitButton = document.querySelector('#submit')
+
+form.addEventListener('submit', (e) => {
+  submitButton.disabled = true
+  e.preventDefault()
+  window.location.href = window.location.origin + '/success.html'
+})
+
+// browser returns to homepage
+setTimeout(() => {
+  window.location.href = window.location.origin
+}, 5000)
